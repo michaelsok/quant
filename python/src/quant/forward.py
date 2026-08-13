7c3953502502cd22d23986620554be5254d6162b
Michaël SOK <mdb.sok@gmail.com>
2026-08-13 21:46:24 +0200

    fix: update BondForward class with correct implementation

--- python/src/quant/forward.py
+++ python/src/quant/forward.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 import numpy as np
 
+from .bonds.zero import ZeroCouponBond
+
 
 @dataclass
 class Forward:
@@ -25,15 +27,19 @@ class Forward:
 
 @dataclass
 class BondForward(Forward):    
-    def get_prices_from(self, lattice, discount):
-        prices = lattice.values[:self.maturity+1, :self.maturity+1].copy()
-        for step in range(lattice.periods - 1, -1, -1):
-            discount_ = discount
-            if isinstance(discount, np.ndarray):
-                discount_ = discount[:step+1, step]
-            prices[:step+1, step] = self.backward(prices[:step+2, step+1], discount_)
-        return prices
+    coupon: float
+    maturation: int
+    _zcb: ZeroCouponBond | None = None
 
-    def get_prices_from(self, value, discount, zcb):
-        
-        return super().get_prices_from(value, discount)
+    @property
+    def zcb(self):
+        if self._zcb is None:
+            self._zcb = ZeroCouponBond(coupon=self.coupon, maturation=self.maturation, q=self.q)
+        return self._zcb
+
+    def get_price_from(self, rates):
+        prices = self.zcb.get_prices_from(rates)
+        discount = 1 / (1 + rates.values)
+        divider = super().get_prices_from(value=1, discount=discount)
+
+        return prices[0, 0] / divider[0, 0]
