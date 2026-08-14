6d364230f5590adc10e6f1fafe821674d4011be6
Michaël SOK <mdb.sok@gmail.com>
2026-08-15 00:28:37 +0200

    feat: add DefaultZeroCouponBond and fix BDT initialization

--- python/src/quant/bonds/zero.py
+++ python/src/quant/bonds/zero.py
@@ -24,3 +24,28 @@ class ZeroCouponBond:
             discount = 1 / rates[:step+1, step]
             prices[:step+1, step] = self.backward(prices[:step+2, step+1], discount=discount)
         return prices
+
+@dataclass
+class DefaultZeroCouponBond:
+    coupon: float
+    maturation: int
+    q: float
+    R: float
+
+    @property
+    def shape(self):
+        return (self.maturation + 1, self.maturation + 1)
+
+    def backward(self, prices, discount, h):
+        non_default = (prices[1:] * self.q + prices[:-1] * (1 - self.q)) * (1 - h)
+        default = h * self.R * self.coupon
+        return (non_default + default) * discount
+
+    def get_prices_from(self, lattice, h):
+        prices = np.zeros(self.shape)
+        prices[:, -1] = self.coupon
+        for step in range(self.maturation - 1, -1, -1):
+            discount = 1 / (1 + lattice.values[:step+1, step])
+            h_ = h[:step+1, step]
+            prices[:step+1, step] = self.backward(prices[:step+2, step+1], discount=discount, h=h_)
+        return prices
--- python/src/quant/models/bdt.py
+++ python/src/quant/models/bdt.py
@@ -10,33 +10,93 @@ from quant.lattices.base import Lattice
 @dataclass
 class BlackDermanToy:
     q: float
-    b: float
     periods: int
+    a: float | None = None
+    b: float | None = None
+    _target: str | None = None
 
-    def get_objective_function_from(self, b, rates, elementary, lattice):
+    @property
+    def target(self):
+        if self._target is None:
+            if self.a is not None:
+                self._target = 'b'
+            elif self.b is not None:
+                self._target = 'a'
+            else:
+                raise ValueError("Cannot define a target")
+        return self._target
+
+    def get_objective_function_from(self, a, b, rates, elementary, lattice):
+        if (a is None) and (b is None):
+            raise ValueError("Either a or b should be passed")
+
         periods = lattice.periods + 1
-        def objective(a):
-            lattice.values = np.triu(a * np.exp(self.b * np.arange(periods))[:, None])
-            prices = elementary.get_prices_from(lattice).sum(axis=0)[1:]
-            predictions = ((1 / prices) ** (1 / (np.arange(periods) + 1)) - 1)
-            return np.sum(np.square(predictions - rates))
+        if a is None:
+            def objective(a):
+                lattice.values = np.triu(a * np.exp(self.b * np.arange(periods))[:, None])
+                prices = elementary.get_prices_from(lattice).sum(axis=0)[1:]
+                predictions = ((1 / prices) ** (1 / (np.arange(periods) + 1)) - 1)
+                return np.sum(np.square(predictions - rates))
+        else:
+            def objective(b):
+                lattice.values = np.triu(self.a * np.exp(b * np.arange(periods))[:, None])
+                prices = elementary.get_prices_from(lattice).sum(axis=0)[1:]
+                predictions = ((1 / prices) ** (1 / (np.arange(periods) + 1)) - 1)
+                return np.sum(np.square(predictions - rates))
         return objective
 
-    def fit(self, rates, initialization: np.ndarray | None = None, seed=None, **kwargs):
-        a = initialization
+    def get_objective_function_from_prices(self, a, b, prices, elementary, lattice):
+        if (a is None) and (b is None):
+            raise ValueError("Either a or b should be passed")
+
+        periods = lattice.periods + 1
+        if a is None:
+            def objective(a):
+                lattice.values = np.triu(a * np.exp(self.b * np.arange(periods))[:, None])
+                predictions = elementary.get_prices_from(lattice).sum(axis=0)[1:]
+                return np.sum(np.square(predictions - prices))
+        else:
+            def objective(b):
+                lattice.values = np.triu(self.a * np.exp(b * np.arange(periods))[:, None])
+                predictions = elementary.get_prices_from(lattice).sum(axis=0)[1:]
+                return np.sum(np.square(predictions - prices))
+        return objective
+
+    def fit(self, rates=None, initialization: np.ndarray | None = None, seed=None, prices=None, **kwargs):
+        x0 = initialization
+
+        if rates is None:
+            target = prices
+        else:
+            target = rates
+
         if initialization is None:
-            a = np.ones_like(rates) * rates[0, 0]
+            x0 = np.ones_like(target) * target[0]
+
+        if self.target == 'a':
+            a, b = None, self.b
+        elif self.target == 'b':
+            a, b = self.a, None
+        else:
+            raise ValueError("Cannot determine target")
 
         elementary = Elementary(q=self.q)
         lattice = Lattice(base=None, periods=self.periods - 1)
-        objective = self.get_objective_function_from(
-            b=self.b, rates=rates,
-            elementary=elementary, lattice=lattice
-        )
-        
+
+        if prices is None:
+            objective = self.get_objective_function_from(
+                a=a, b=b, rates=rates,
+                elementary=elementary, lattice=lattice
+            )
+        else:
+            objective = self.get_objective_function_from_prices(
+                a=a, b=b, prices=prices,
+                elementary=elementary, lattice=lattice
+            )
+
         np.random.seed(seed)
-        self.report = minimize(objective, a, **kwargs)
-        self.a = self.report.x
+        self.report = minimize(objective, x0, **kwargs)
+        setattr(self, self.target, self.report.x)
         return self
 
     def get_rates(self):
@@ -44,17 +104,3 @@ class BlackDermanToy:
         lattice = Lattice(base=rates[0, 0], periods=self.periods - 1)
         lattice.values = rates
         return lattice
-
-
-
-if __name__ == '__main__':
-    periods = 14
-    rates = np.array([7.3, 7.62, 8.1, 8.45, 9.2, 9.64, 10.12, 10.45, 10.75, 11.22, 11.55, 11.92, 12.2, 12.32]) / 100
-    a = np.ones_like(rates) * .05
-    b = .005
-    q = .5
-
-    bdt = BlackDermanToy(q=q, b=b, periods=periods)
-    bdt.fit(rates, initialization=a, options={'disp': True})
-    import ipdb; ipdb.set_trace()
-
